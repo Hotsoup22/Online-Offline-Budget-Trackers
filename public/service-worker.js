fb56e6ded6179b94c5c2eb2bf8938b3151a2134e
hotsoup42 <jose22gw@gmail.com>
2021-09-29 14:24:38 -0700

    fixed mongoosd server.js connection, also had GET 404 errors for service worker and manifest, now fixed.

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,15 +3,16 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/styles.css",
   "/index.js",
-  "manifest.webmanifest",
+  "/manifest/webAppManifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
+
 ];
 const PRE_CACHE = "precache-v1";
 const RUNTIME = "runtime";
 // Install
-self.addEventListener("install", function (evt) {
-  evt.waitUntil(
+self.addEventListener("install", function (event) {
+  event.waitUntil(
     caches.open(PRE_CACHE).then((cache) => {
       console.log("Your files were pre-cached successfully!");
       return cache.addAll(FILES_TO_CACHE);
@@ -21,8 +22,8 @@ self.addEventListener("install", function (evt) {
   self.skipWaiting();
 });
 // The activate handler takes care of cleaning up old caches.
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
@@ -39,22 +40,22 @@ self.addEventListener("activate", function (evt) {
 });
 
 // Fetch
-self.addEventListener("fetch", function (evt) {
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
+self.addEventListener("fetch", function (event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
       caches
         .open(RUNTIME)
         .then((cache) => {
-          return fetch(evt.request)
+          return fetch(event.request)
             .then((response) => {
               if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
+                cache.put(event.request.url, response.clone());
               }
 
               return response;
             })
             .catch((err) => {
-              return cache.match(evt.request);
+              return cache.match(event.request);
             });
         })
         .catch((err) => console.log(err))
@@ -62,9 +63,9 @@ self.addEventListener("fetch", function (evt) {
 
     return;
   }
-  evt.respondWith(
-    caches.match(evt.request).then(function (response) {
-      return response || fetch(evt.request);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
     })
   );
 });
