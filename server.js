a0931ee165b71d853edb1e545b1ade6bce5816f8
hotsoup42 <jose22gw@gmail.com>
2021-10-01 10:20:48 -0700

    server.js updated app.listen console.log

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget",
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen( PORT, () => {
+	console.log( `App running http://localhost:${PORT}` );
 });
